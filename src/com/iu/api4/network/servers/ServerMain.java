bca255602ee35578b6c3aff080f64a0d48fd0b52
kimjiwon-0821 <wldnjsqkek29@naver.com>
2023-01-13 14:21:12 +0900

    second

---
+++ src/com/iu/api4/network/servers/ServerMain.java
@@ -0,0 +1,130 @@
+package com.iu.api4.network.servers;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import com.iu.api2.collections.ex1.StudentDTO;
+
+public class ServerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServerSocket ss = null;
+		Socket sc = null;
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		BufferedWriter bw = null;
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+		
+		try {
+//서버 접속
+			ss = new ServerSocket(8282);
+			System.out.println("client 접속 대기중...");
+			sc = ss.accept();
+			System.out.println("client 접속 성공");
+
+//파일접속
+			File file = new File("C:\\fileTest");
+			String [] names = file.list();
+			long max = 0;
+			for(String name:names) {
+				name = name.substring(0,name.lastIndexOf("."));
+				long data = Long.parseLong(name);
+				if(data>max)
+					max = data;
+			}
+			file = new File(file,max+".txt");
+			System.out.println(file);	
+//학생정보초기화
+			ArrayList<StudentDTO> ar = new ArrayList<>();
+			FileReader fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			while(true) {
+				String data = br.readLine();
+				if(data==null)
+					break;
+				StringTokenizer st = new StringTokenizer(data,"-");
+				StudentDTO sd = new StudentDTO();
+				sd.setName(st.nextToken());
+				sd.setNum(Integer.parseInt(st.nextToken()));
+				sd.setKor(Integer.parseInt(st.nextToken()));
+				sd.setEng(Integer.parseInt(st.nextToken()));
+				sd.setMath(Integer.parseInt(st.nextToken()));
+				sd.setTotal(Integer.parseInt(st.nextToken()));
+				sd.setAvg(Double.parseDouble(st.nextToken()));
+				ar.add(sd);
+			}
+			
+			
+//입력받은기능정보
+			is = sc.getInputStream();
+			ir = new InputStreamReader(is);
+			br = new BufferedReader(ir);
+			String select = br.readLine();
+			//int num = Integer.parseInt(select.substring(select.lastIndexOf(",")));
+			int num = Integer.parseInt(select);
+			switch(num) {
+//전체학생정보 전송
+			case 1:
+				os = sc.getOutputStream();
+				ow = new OutputStreamWriter(os);
+				bw = new BufferedWriter(ow);
+				for(StudentDTO studentDTO:ar) {
+					bw.write("Name : "+studentDTO.getName()+"\r\n");
+					bw.write("NUM : "+studentDTO.getNum()+"\r\n");
+					bw.write("Kor : "+studentDTO.getKor()+"\r\n");
+					bw.write("Eng : "+studentDTO.getEng()+"\r\n");
+					bw.write("Math : "+studentDTO.getMath()+"\r\n");
+					bw.write("Total : "+studentDTO.getTotal()+"\r\n");
+					bw.write("Avg : "+studentDTO.getAvg()+"\r\n");
+					bw.flush();
+					
+				}
+				bw.write("모든 학생 정보가 출력되었습니다.");
+				bw.flush();
+				break;
+			
+//학생정보검색 출력
+			case 2:
+				is = sc.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+				String name = br.readLine();
+				for(StudentDTO studentDTO : ar) {
+					if(name.equals(studentDTO.getName())) {
+						bw.write("Name : "+studentDTO.getName()+"\r\n");
+						bw.write("NUM : "+studentDTO.getNum()+"\r\n");
+						bw.write("Kor : "+studentDTO.getKor()+"\r\n");
+						bw.write("Eng : "+studentDTO.getEng()+"\r\n");
+						bw.write("Math : "+studentDTO.getMath()+"\r\n");
+						bw.write("Total : "+studentDTO.getTotal()+"\r\n");
+						bw.write("Avg : "+studentDTO.getAvg()+"\r\n");
+						bw.flush();
+						break;
+					}
+					}
+				bw.write("모든 학생 정보가 출력되었습니다.");
+				bw.flush();
+				break;
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
